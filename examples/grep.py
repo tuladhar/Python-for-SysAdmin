696f9be68f335ff484244dd19bfe1a6ef5e72b9d
pushtakio <alex@vaiolabs.com>
2019-10-14 18:17:44 +0300

    adding grep script

---
+++ examples/grep.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# grep.py
+
+import sys
+
+script = sys.argv[0]
+
+def print_usage():
+	sys.exit('Usage: python {} pattern'.format(script))
+
+def main(argv):
+	if not len(argv) == 1:
+		print_usage()
+
+	pattern = argv[0]
+
+	for line in sys.stdin:
+		if pattern in line:
+			print line.strip()
+
+if __name__ == '__main__':
+	main(sys.argv[1:])
